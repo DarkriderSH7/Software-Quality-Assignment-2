9d761a29349348b529f7f477060877f9258a3b97
DarkriderSH7 <suhaib.hussain26000@gmail.com>
2024-03-06 17:19:31 -0500

    Second Commit

--- Syed Suhaib_Hussain_Assignment 2/Class1.cs
+++ Syed Suhaib_Hussain_Assignment 2/Class1.cs
@@ -20,6 +20,49 @@ namespace TriangleSolver
             Assert.That(result, Is.EqualTo("Equilateral triangle"));
         }
 
-        
+        [Test]
+        public void AnalyzeTriangle_Sides5And5And3_IsIsosceles()
+        {
+            // Arrange
+            int firstSide = 5;
+            int secondSide = 5;
+            int thirdSide = 3;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Isosceles triangle"));
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Sides10And10And2_IsIsosceles()
+        {
+            // Arrange
+            int firstSide = 10;
+            int secondSide = 10;
+            int thirdSide = 2;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Isosceles triangle"));
+        }
+
+        [Test]
+        public void AnalyzeTriangle_Sides8And4And8_IsIsosceles()
+        {
+            // Arrange
+            int firstSide = 8;
+            int secondSide = 4;
+            int thirdSide = 8;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Isosceles triangle"));
+        }
     }
 }
