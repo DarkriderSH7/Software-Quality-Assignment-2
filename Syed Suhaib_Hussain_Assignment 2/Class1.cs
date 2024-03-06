777029c53becf2fccc2525c9945ee95303eb748c
DarkriderSH7 <suhaib.hussain26000@gmail.com>
2024-03-06 14:39:09 -0500

    First Commit

---
+++ Syed Suhaib_Hussain_Assignment 2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace TriangleSolver
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void AnalyzeTriangle_InputIsEquilateral_ReturnsEquilateral()
+        {
+            // Arrange
+            int firstSide = 5;
+            int secondSide = 5;
+            int thirdSide = 5;
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.That(result, Is.EqualTo("Equilateral triangle"));
+        }
+
+        
+    }
+}
